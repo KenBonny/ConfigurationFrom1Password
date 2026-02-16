8ceb3f8d70be5b3dd4495b296272c6b637f8fcfe
Ken Bonny <github@ken.morethancode.be>
2026-02-16 20:52:12 +0100

    Add `ConfigurationFrom1Password.Console` project to solution

---
+++ ConfigurationFrom1Password.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConfigurationFrom1Password;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration
+    .AddInMemoryCollection([new("InMemorySensitiveData", "op://Private/1Password Config Extension Test/password")])
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Replace1PasswordSecrets();
+
+Console.WriteLine(builder.Configuration["InMemorySensitiveData"]);
+Console.WriteLine(builder.Configuration["SensitiveData"]);
+Console.WriteLine(builder.Configuration["ExternalService"]);
+Console.WriteLine(builder.Configuration.GetConnectionString("DefaultConnection"));

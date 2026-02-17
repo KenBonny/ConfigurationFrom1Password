4436db3e23a850f460ecfcad6f0d1085e05e670f
Ken Bonny <github@ken.morethancode.be>
2026-02-17 17:49:56 +0100

    Add test project for `ConfigurationFrom1Password` with initial setup
    Add unit tests for testing secret replacement and mixed data handling in 1Password configuration extension
    Add test infrastructure for 1Password configuration extension with secret replacement

---
+++ ConfigurationFrom1Password.Tests/LoadSettingsFrom1Password.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace ConfigurationFrom1Password.Tests;
+
+public class LoadSettingsFrom1Password
+{
+    [Test]
+    public async Task ReplaceSingleSetting()
+    {
+        var dict = new Dictionary<string, string>
+        {
+            ["op://Private/1Password Config Extension Test/password"] = "Test Password w17h #\n",
+            ["op://Private/1Password Config Extension Test/url"] = "https://www.google.com\n",
+            ["op://Private/1Password Config Extension Test/connectionstring"] = "connection string value to access a database\n"
+        };
+        
+        var builder = Host.CreateApplicationBuilder();
+
+        builder.Configuration
+            .AddInMemoryCollection([new ("SensitiveData", "op://Private/1Password Config Extension Test/password")])
+            .Replace1PasswordSecretsForTesting(key => dict[key]);
+        var secret = builder.Configuration["SensitiveData"];
+        await Assert.That(secret).IsNotNull().And.IsEqualTo("Test Password w17h #");
+    }
+    
+    [Test]
+    public async Task Replace1PasswordSecretsButNotOthers()
+    {
+        var dict = new Dictionary<string, string>
+        {
+            ["op://Private/1Password Config Extension Test/password"] = "Test Password w17h #\n",
+            ["op://Private/1Password Config Extension Test/url"] = "https://www.google.com\n",
+            ["op://Private/1Password Config Extension Test/connectionstring"] = "connection string value to access a database\n"
+        };
+
+        var builder = Host.CreateApplicationBuilder();
+        builder.Configuration.AddInMemoryCollection([new ("SensitiveData", "op://Private/1Password Config Extension Test/password"),
+            new ("NonSensitiveData", "non-sensitive value")])
+            .Replace1PasswordSecretsForTesting(key => dict[key]);
+        
+        var secret = builder.Configuration["SensitiveData"];
+        await Assert.That(secret).IsNotNull().And.IsEqualTo("Test Password w17h #");
+        var nonSensitive = builder.Configuration["NonSensitiveData"];
+        await Assert.That(nonSensitive).IsNotNull().And.IsEqualTo("non-sensitive value");
+    }
+}
---
+++ ConfigurationFrom1Password.Tests/TestRegistrations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConfigurationFrom1Password.Tests;
+
+public static class OnePasswordTestConfigurationExtension
+{
+    public static IConfigurationBuilder Replace1PasswordSecretsForTesting(this IConfigurationBuilder builder, Func<string, string> getSecretsForTesting)
+    {
+        builder.Add(new OnePasswodTestConfigurationSource(getSecretsForTesting));
+        return builder;
+    }
+}
+
+public class OnePasswodTestConfigurationSource(Func<string, string> getSecretsForTesting) : IConfigurationSource
+{
+    /// <inheritdoc />
+    public IConfigurationProvider Build(IConfigurationBuilder builder) =>
+        new OnePasswordConfigurationProvider(builder.Build(), getSecretsForTesting);
+}

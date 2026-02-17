f7de0b3ad77b3cd977938de71ce1b01fbbc6b19f
Ken Bonny <github@ken.morethancode.be>
2026-02-17 18:43:26 +0100

    Add `RoughSpeedCheckTest` to evaluate basic loading performance from 1Password configuration

---
+++ ConfigurationFrom1Password.Tests/RoughSpeedCheckTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace ConfigurationFrom1Password.Tests;
+
+public class RoughSpeedCheckTest
+{
+    [Test]
+    public Task LoadFrom1Password()
+    {
+        var builder = Host.CreateApplicationBuilder();
+        builder.Configuration.AddJsonFile("LoadSettingsFrom1Password.json").Replace1PasswordSecrets();
+        return Task.CompletedTask;
+    }
+}

8fc6cf96f5cc7e367ab45c879c5e2698bd55925d
Ken Bonny <github@ken.morethancode.be>
2026-02-17 18:03:28 +0100

    Refactor and reorganize `ConfigurationFrom1Password.Tests` by consolidating repeated test data into `TestData` and simplifying test implementations for improved maintainability.

---
+++ ConfigurationFrom1Password.Tests/LoadSettings.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace ConfigurationFrom1Password.Tests;
+
+public class LoadSettings
+{
+    [Test]
+    public async Task ReplaceSingleSetting()
+    {
+        var builder = Host.CreateApplicationBuilder();
+        builder.Configuration
+            .AddInMemoryCollection([new ("SensitiveData", "op://Private/1Password Config Extension Test/password")])
+            .Replace1PasswordSecretsForTesting(TestData.GetSecretsForTesting);
+
+        await builder.Configuration.AreEqual("SensitiveData", TestData.Password);
+    }
+    
+    [Test]
+    public async Task Replace1PasswordSecretsButNotOthers()
+    {
+        var builder = Host.CreateApplicationBuilder();
+        builder.Configuration
+            .AddInMemoryCollection(
+            [
+                new("SensitiveData", "op://Private/1Password Config Extension Test/password"),
+                new("NonSensitiveData", "non-sensitive value")
+            ])
+            .Replace1PasswordSecretsForTesting(TestData.GetSecretsForTesting);
+        
+        await builder.Configuration.AreEqual("SensitiveData", TestData.Password);
+        await builder.Configuration.AreEqual("NonSensitiveData", "non-sensitive value");
+    }
+}
---
+++ ConfigurationFrom1Password.Tests/TestData.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Frozen;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfigurationFrom1Password.Tests;
+
+public static class OnePasswordTestConfigurationExtension
+{
+    public static IConfigurationBuilder Replace1PasswordSecretsForTesting(this IConfigurationBuilder builder, Func<string, string> getSecretsForTesting)
+    {
+        builder.Add(new OnePasswodTestConfigurationSource(getSecretsForTesting));
+        return builder;
+    }
+}
+
+public class OnePasswodTestConfigurationSource(Func<string, string> getSecretsForTesting) : IConfigurationSource
+{
+    /// <inheritdoc />
+    public IConfigurationProvider Build(IConfigurationBuilder builder) =>
+        new OnePasswordConfigurationProvider(builder.Build(), getSecretsForTesting);
+}
+
+public static class TestData
+{
+    public const string Password = "Test Password w17h #";
+    public const string TestUrl = "https://www.google.com";
+    public const string TestConnectionString = "connection string value to access a database";
+
+    private static readonly FrozenDictionary<string, string> Secrets = new Dictionary<string, string>()
+    {
+        ["op://Private/1Password Config Extension Test/password"] = Password + Environment.NewLine,
+        ["op://Private/1Password Config Extension Test/url"] = TestUrl + Environment.NewLine,
+        ["op://Private/1Password Config Extension Test/connectionstring"] =
+            TestConnectionString + Environment.NewLine
+    }.ToFrozenDictionary();
+    
+    public static Func<string, string> GetSecretsForTesting = key => Secrets[key];
+
+    extension(IConfiguration config)
+    {
+        public async Task AreEqual(string key, string value) => await Assert.That(config[key]).IsEqualTo(value); 
+    }
+}

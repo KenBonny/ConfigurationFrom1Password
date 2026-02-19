af3ac89bcf9d284f3e786bfe88863a44bc81b90d
Ken Bonny <github@ken.morethancode.be>
2026-02-19 18:09:07 +0100

    Add `BeforeAllTests` to handle 1Password login for test secret access

---
+++ ConfigurationFrom1Password.Tests/BeforeAllTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace ConfigurationFrom1Password.Tests;
+
+public class BeforeAllTests
+{
+    [Before(Assembly)]
+    public static void LogInTo1PasswordSoAllTestsCanAccessSecrets()
+    {
+        var logIn1PasswordInfo = new ProcessStartInfo("op.exe", ["signin"])
+        {
+            CreateNoWindow = true,
+            UseShellExecute = false,
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+        };
+        var logIn1PasswordProcess = Process.Start(logIn1PasswordInfo);
+        
+        if (logIn1PasswordProcess is null)
+            throw new InvalidOperationException("Failed to start op.exe");
+        
+        logIn1PasswordProcess.WaitForExit();
+    }
+}
